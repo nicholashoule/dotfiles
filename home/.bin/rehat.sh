58c8c94198b4b07f0a333e46a6cc08294beb382a
Nicholas Houle <nicholashoule@users.noreply.github.com>
2016-08-01 09:44:28 -0600

    Update rehat.sh

--- home/.bin/rehat.sh
+++ home/.bin/rehat.sh
@@ -1,1 +1,15 @@
-# [Placeholder] Delete me.
+#!/bin/bash
+echo "Idle users will be removed after 15 minutes"
+echo "readonly TMOUT=900" >> /etc/profile.d/os-security.sh
+echo "readonly HISTFILE" >> /etc/profile.d/os-security.sh
+chmod +x /etc/profile.d/os-security.sh
+
+echo "Locking down Cron"
+touch /etc/cron.allow
+chmod 600 /etc/cron.allow
+awk -F: '{print $1}' /etc/passwd | grep -v root > /etc/cron.deny
+
+echo "Locking down @AT"
+touch /etc/at.allow
+chmod 600 /etc/at.allow
+awk -F: '{print $1}' /etc/passwd | grep -v root > /etc/at.deny
