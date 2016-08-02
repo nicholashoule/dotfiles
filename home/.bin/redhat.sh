b0425d5970b755ad9cd0f2818dc34f64ae9346a9
Nicholas Houle <nicholashoule@users.noreply.github.com>
2016-08-02 12:35:45 -0600

    Update redhat.sh

--- home/.bin/redhat.sh
+++ home/.bin/redhat.sh
@@ -184,6 +184,10 @@ service ftp
 }
 EOF
 
+bash.utils.consoleLog 'Install RVM:'
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+\curl -sSL https://get.rvm.io | bash -s stable
+
 }
 #--------------------------------------------
 # Source bash utils functions and call main()
