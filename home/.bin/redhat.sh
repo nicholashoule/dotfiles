c471c4d8ee01840f4264cc1873a20443a33b2b9a
Nicholas Houle <nicholashoule@users.noreply.github.com>
2016-08-02 15:32:09 -0600

    Update redhat.sh

--- home/.bin/redhat.sh
+++ home/.bin/redhat.sh
@@ -185,10 +185,10 @@ service ftp
 EOF
 
 bash.utils.consoleLog 'Install RVM:'
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+\curl -sSL https://rvm.io/mpapis.asc | sudo gpg2 --import -
 \curl -sSL https://get.rvm.io | bash -s stable
 
-bash.utils.consoleLog 'Set git credential to use gnome-keyring:'
+bash.utils.consoleLog 'Set git credential to use gnome-keyring.'
 git config --global credential.helper gnome-keyring
 
 }
