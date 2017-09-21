b459af8f7e4c251d284f3edcf51b4c2e3137791e
Nicholas Houle <nicholashoule@users.noreply.github.com>
2017-09-21 16:16:22 -0600

    Update oh-my-zsh.sh

--- home/.sh/oh-my-zsh.sh
+++ home/.sh/oh-my-zsh.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+# git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 export ZSH="$HOME/.dotfiles/oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
